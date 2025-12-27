319fce3d105a6c1a349f7277dced178c7dd79d16
kapilarny <kapilarnyyt@gmail.com>
2025-12-27 19:38:47 +0100

    add missing defines

--- japi_project_create.py
+++ japi_project_create.py
@@ -46,6 +46,10 @@ plugin_h_content = """#ifndef JAPI_REWRITE_TESTPLUGIN_PLUGIN_H
 EXPORT JAPIModMeta __stdcall GetModMeta();
 EXPORT void __stdcall ModInit();
 
+#define JFATAL(fmt, ...) JAPI_LogMessage(JAPI_LOG_LEVEL_FATAL, fmt, ##__VA_ARGS__)
+#define JERROR(fmt, ...) JAPI_LogMessage(JAPI_LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+#define JDEBUG(fmt, ...) JAPI_LogMessage(JAPI_LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#define JWARN(fmt, ...) JAPI_LogMessage(JAPI_LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
 #define JINFO(fmt, ...) JAPI_LogMessage(JAPI_LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
 
 #endif //JAPI_REWRITE_TESTPLUGIN_PLUGIN_H"""
